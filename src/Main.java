f52eed98dce2b1b01dbcfb6996eaadd2e0504eeb
abdul <abdulsamad7239@gmail.com>
2024-04-28 19:12:41 +0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame pos = new JFrame("Welcome to ActivePOS");
+        pos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pos.setExtendedState(JFrame.MAXIMIZED_BOTH); //open the window in fulwidth
+
+        ImageIcon image = new ImageIcon("as.png");
+        pos.setIconImage(image.getImage());
+        pos.getContentPane().setBackground(new Color(255,255,250));
+
+
+
+        pos.setVisible(true);
+
+        }
+    }
